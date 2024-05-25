0286e5bb5c769d0ef6c4f252f7baea3722854fc9
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-26 03:23:09 +0530

    creating search controller

--- server/controllers/search.controller.js
+++ server/controllers/search.controller.js
@@ -21,3 +21,23 @@ export const searchMoviecontroller = async (req, res) => {
     })
   }
 };
+
+
+export const suggestedMoviescontroller = async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=Inception&page=2`
+    )
+
+    return res.status(200).json({
+      success: true,
+      message: "Movie find successfully",
+      movies: data
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error searching movies"
+    })
+  }
+}
