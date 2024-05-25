93283ce245834ccfa58d24c91db81a21beff9878
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-26 03:25:17 +0530

    creating a homePage

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import HomeSvg from "../assets/HomeSvg.svg";
+import {Link} from 'react-router-dom'
+import {Context} from '../main'
+
+const HomePage = () => {
+  const {isAuthenticated}= useContext(Context)
+
+  return (
+    <div className="h-[500px] md:h-[615px] bg-[#118ab2] flex items-center justify-center">
+      <div className="pt-24 md:pt-0">
+        <img src={HomeSvg} alt="HomeSvg" />
+        <Link
+        to={
+          isAuthenticated ? "/home" : "/signup"
+        }
+        className="py-2 px-6 border-2 mt-10 ml-36 
+        md:ml-[300px] rounded-md text-white bg-pink-500">
+          Get Started
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
