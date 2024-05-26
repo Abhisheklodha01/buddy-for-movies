778c54ffc553beeaad6a4dee56f44ff5532ac583
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-27 01:05:34 +0530

    creating movies details controller and route

--- server/controllers/search.controller.js
+++ server/controllers/search.controller.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export const searchMoviecontroller = async (req, res) => {
   const { query } = req.query;
@@ -12,70 +12,89 @@ export const searchMoviecontroller = async (req, res) => {
     return res.status(200).json({
       success: true,
       message: "Movie find successfully",
-      movies: data
-    })
+      movies: data,
+    });
   } catch (err) {
     return res.status(500).json({
       success: false,
-      message: "Error searching movies"
-    })
+      message: "Error searching movies",
+    });
   }
 };
-
 
 export const suggestedMoviescontroller = async (req, res) => {
   try {
     const { data } = await axios.get(
       `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=Bollywood&page=1`
-    )
+    );
 
     return res.status(200).json({
       success: true,
       message: "Movie find successfully",
-      movies: data
-    })
+      movies: data,
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
-      message: "Error searching movies"
-    })
+      message: "Error searching movies",
+    });
   }
-}
+};
 
 export const suggestedMoviescontroller2 = async (req, res) => {
   try {
     const { data } = await axios.get(
       `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=Bollywood&page=2`
-    )
+    );
 
     return res.status(200).json({
       success: true,
       message: "Movie find successfully",
-      movies: data
-    })
+      movies: data,
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
-      message: "Error searching movies"
-    })
+      message: "Error searching movies",
+    });
   }
-}
+};
 
 export const suggestedMoviescontroller3 = async (req, res) => {
   try {
     const { data } = await axios.get(
       `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=Bollywood&page=3`
-    )
+    );
 
     return res.status(200).json({
       success: true,
       message: "Movie find successfully",
-      movies: data
-    })
+      movies: data,
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
-      message: "Error searching movies"
-    })
+      message: "Error searching movies",
+    });
   }
-}
+};
+
+export const getMovieDescription = async (req, res) => {
+  const { movieId } = req.body;
+  try {
+    const { data } = await axios.get(
+      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&i=${movieId}`
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Movie details find successfully",
+      movie: data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "internal server error",
+    });
+  }
+};
--- server/routes/search.route.js
+++ server/routes/search.route.js
@@ -1,11 +1,17 @@
-import express from 'express'
-import { searchMoviecontroller, suggestedMoviescontroller, suggestedMoviescontroller2 } from '../controllers/search.controller.js'
+import express from "express";
+import {
+  getMovieDescription,
+  searchMoviecontroller,
+  suggestedMoviescontroller,
+  suggestedMoviescontroller2,
+} from "../controllers/search.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/search", searchMoviecontroller)
-router.get("/getmovies", suggestedMoviescontroller)
-router.get("/getmovies2", suggestedMoviescontroller2)
-router.get("/getmovies3", suggestedMoviescontroller2)
+router.get("/search", searchMoviecontroller);
+router.get("/getmovies", suggestedMoviescontroller);
+router.get("/getmovies2", suggestedMoviescontroller2);
+router.get("/getmovies3", suggestedMoviescontroller2);
+router.get("/getdetails", getMovieDescription);
 
-export default router
+export default router;
