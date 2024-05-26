eefef028694fde4e7e9f0ee46b5ece74a4860c3a
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-27 00:16:51 +0530

    adding bollywood movies

--- server/controllers/search.controller.js
+++ server/controllers/search.controller.js
@@ -26,7 +26,45 @@ export const searchMoviecontroller = async (req, res) => {
 export const suggestedMoviescontroller = async (req, res) => {
   try {
     const { data } = await axios.get(
-      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=Inception&page=2`
+      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=Bollywood&page=1`
+    )
+
+    return res.status(200).json({
+      success: true,
+      message: "Movie find successfully",
+      movies: data
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error searching movies"
+    })
+  }
+}
+
+export const suggestedMoviescontroller2 = async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=Bollywood&page=2`
+    )
+
+    return res.status(200).json({
+      success: true,
+      message: "Movie find successfully",
+      movies: data
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error searching movies"
+    })
+  }
+}
+
+export const suggestedMoviescontroller3 = async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=Bollywood&page=3`
     )
 
     return res.status(200).json({
--- server/routes/search.route.js
+++ server/routes/search.route.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { searchMoviecontroller, suggestedMoviescontroller } from '../controllers/search.controller.js'
+import { searchMoviecontroller, suggestedMoviescontroller, suggestedMoviescontroller2 } from '../controllers/search.controller.js'
 
 const router = express.Router()
 
 router.get("/search", searchMoviecontroller)
 router.get("/getmovies", suggestedMoviescontroller)
+router.get("/getmovies2", suggestedMoviescontroller2)
+router.get("/getmovies3", suggestedMoviescontroller2)
 
 export default router
