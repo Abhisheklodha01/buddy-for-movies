222d1028993cdc3ed9d26dd4d0cdf9bc623d6d77
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-27 00:18:45 +0530

    solving Get started button allingment

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -10,11 +10,15 @@ const HomePage = () => {
     <div className="h-[500px] md:h-[615px] bg-[#118ab2] flex items-center justify-center">
       <div className="pt-24 md:pt-0">
         <img src={HomeSvg} alt="HomeSvg" />
+        <div className="sm:hidden">
+        <br />
+        <br />
+        </div>
         <Link
         to={
           isAuthenticated ? "/home" : "/signup"
         }
-        className="py-2 px-6 border-2 mt-10 ml-36 
+        className="py-2 px-6 border-2 mt-10 ml-20 
         md:ml-[300px] rounded-md text-white bg-pink-500">
           Get Started
         </Link>
