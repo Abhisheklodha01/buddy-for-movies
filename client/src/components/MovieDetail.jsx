37b1524c550b0acb1e0e442ae71a419e6daeaba9
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-29 00:26:10 +0530

    adding one more backend api call

--- client/src/components/MovieDetail.jsx
+++ client/src/components/MovieDetail.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { server } from "../utils/constants.js";
+import { toast } from "react-hot-toast";
 
 const MovieDetailPage = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
+  const [present, setPresent] = useState(false);
+  const [movieid, setMovieid] = useState("");
+  const navigate = useNavigate();
+
   useEffect(() => {
     const getMovieDetail = async () => {
       try {
@@ -56,6 +61,59 @@ const MovieDetailPage = () => {
     }
   };
 
+  useEffect(() => {
+    const checkmovieIsInList = async () => {
+      try {
+        const { data } = await axios.post(
+          `${server}/movies/checkmovie`,
+          {
+            imdbId: movieId,
+          },
+          {
+            headers: {
+              "Content-Length": "application/json",
+            },
+            withCredentials: true,
+          }
+        );
+        if (data.success === true) {
+          setPresent(true);
+          setMovieid(data.movie);
+        } else {
+          setPresent(false);
+        }
+      } catch (error) {
+        setPresent(false);
+      }
+    };
+    checkmovieIsInList();
+  }, []);
+
+  const DeleteMovieFromList = async () => {
+    try {
+      const { data } = await axios.post(
+        `${server}/movieslists/deletemovie`,
+        {
+          movieId: movieid._id,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      toast.success(data.message, {
+        position: "top-center",
+      });
+      navigate("/playlist");
+    } catch (error) {
+      toast.error(error.response.data.message, {
+        position: "top-center",
+      });
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gray-800 text-white pb-20 md:pt-10">
       <div
@@ -80,15 +138,32 @@ const MovieDetailPage = () => {
           <p>Writer: {movie?.Writer}</p>
           <p>Imdb Rating: {movie?.Rating}</p>
           <p>Imdb Votes: {movie?.Votes}</p>
-          <button
-            onClick={() =>
-              addToList(movie?.Poster, movie?.Released, movie?.Title, movieId)
-            }
-            className="bg-green-600 rounded-md border-2
+          <div>
+            {present === false ? (
+              <button
+                onClick={() =>
+                  addToList(
+                    movie?.Poster,
+                    movie?.Released,
+                    movie?.Title,
+                    movieId
+                  )
+                }
+                className="bg-green-600 rounded-md border-2
+             border-green-600 pl-5 pr-5 mt-2 py-1"
+              >
+                Add To PlayList
+              </button>
+            ) : (
+              <button
+                onClick={DeleteMovieFromList}
+                className="bg-green-600 rounded-md border-2
            border-green-600 pl-5 pr-5 mt-2 py-1"
-          >
-            Add To PlayList
-          </button>
+              >
+                Delete From PlayList
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </div>
